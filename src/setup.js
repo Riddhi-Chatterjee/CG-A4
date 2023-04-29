1235a2b62c7455f81e2579f94431a8a4d3040b4b
Riddhi-Chatterjee <riddhich@rediffmail.com>
2023-04-30 03:21:51 +0530

    updates

--- src/setup.js
+++ src/setup.js
@@ -10,19 +10,19 @@ export class Setup
     {
         this.animationSetup = new THREE.Object3D(); //Only this is to be added to the scene
 
-        this.sphere1_radius = 0.17;
+        this.sphere1_radius = 0.1;
         this.sphere1_texture = new THREE.TextureLoader().load('/textures/sphere.jpg'); 
         this.sphere1_material = new THREE.MeshPhongMaterial({map: this.sphere1_texture, emissive: 0xffffff, emissiveIntensity: 0.1});
         this.sphere1_geometry = new THREE.SphereGeometry(this.sphere1_radius, 40, 40);
         this.sphere1 = new THREE.Mesh(this.sphere1_geometry, this.sphere1_material);
 
-        this.sphere2_radius = 0.17;
+        this.sphere2_radius = 0.1;
         this.sphere2_texture = new THREE.TextureLoader().load('/textures/sphere.jpg'); 
         this.sphere2_material = new THREE.MeshPhongMaterial({map: this.sphere2_texture, emissive: 0xffffff, emissiveIntensity: 0.1});
         this.sphere2_geometry = new THREE.SphereGeometry(this.sphere2_radius, 40, 40);
         this.sphere2 = new THREE.Mesh(this.sphere2_geometry, this.sphere2_material);
         
-        this.sphere3_radius = 0.17;
+        this.sphere3_radius = 0.1;
         this.sphere3_texture = new THREE.TextureLoader().load('/textures/sphere.jpg'); 
         this.sphere3_material = new THREE.MeshPhongMaterial({map: this.sphere3_texture, emissive: 0xffffff, emissiveIntensity: 0.1});
         this.sphere3_geometry = new THREE.SphereGeometry(this.sphere3_radius, 40, 40);
@@ -57,8 +57,17 @@ export class Setup
         this.wall_geometry = new THREE.BoxGeometry(this.wall_width, this.wall_height, this.wall_length);
         this.wall = new THREE.Mesh(this.wall_geometry, this.wall_material);
 
-        this.nail1 = (new Nail([0.0,0.0,0.0], 0.01, 0.015, 0.02, 0.005, [1.0,1.0,1.0])).axis
-        this.nail2 = (new Nail([0.0,0.0,0.0], 0.01, 0.015, 0.02, 0.005, [1.0,1.0,1.0])).axis
+        this.nail1_axis_radius = 0.01
+        this.nail1_head_radius = 0.02
+        this.nail1_axis_length = 0.04
+        this.nail1_head_length = 0.005
+        this.nail1 = (new Nail([0.0,0.0,0.0], this.nail1_axis_radius, this.nail1_head_radius, this.nail1_axis_length, this.nail1_head_length, [1.0,1.0,1.0])).axis
+
+        this.nail2_axis_radius = 0.01
+        this.nail2_head_radius = 0.02
+        this.nail2_axis_length = 0.04
+        this.nail2_head_length = 0.005 
+        this.nail2 = (new Nail([0.0,0.0,0.0], this.nail2_axis_radius, this.nail2_head_radius, this.nail2_axis_length, this.nail2_head_length, [1.0,1.0,1.0])).axis
 
         this.nail3_axis_radius = 1.1*(this.vert_box_width/2);
         this.nail3_head_radius = 1.5*(this.vert_box_width/2);
@@ -72,9 +81,11 @@ export class Setup
         this.nail4_head_length = 0.01;
         this.nail4 = (new Nail([0.0,0.0,0.0], this.nail4_axis_radius, this.nail4_head_radius, this.nail4_axis_length, this.nail4_head_length, [1.0,1.0,1.0])).axis
 
+        this.rod_radius = 0.01
+        this.rod_height = 0.5
         this.rod_texture = new THREE.TextureLoader().load('/textures/rod_cuboid.jpg');
         this.rod_material = new THREE.MeshPhongMaterial({map: this.rod_texture});
-        this.rod_geometry = new THREE.CylinderGeometry();
+        this.rod_geometry = new THREE.CylinderGeometry(this.rod_radius, this.rod_radius, this.rod_height, 40, 40);
         this.rod = new THREE.Mesh(this.rod_geometry, this.rod_material);
 
         this.rope_texture = new THREE.TextureLoader().load('/textures/rope.jpg');
@@ -100,6 +111,16 @@ export class Setup
         this.nail3.rotation.x = Math.PI
         this.nail3.position.set(0.0, -1*this.vert_box_length/2, 0.0)
 
+        this.sphere3.position.set(0.6*(this.floor_breadth/2), -1.0 + this.sphere3_radius + this.vert_box_height + this.floor_depth/2, 0.0)
+
+        this.nail2.position.set(0.6*(this.floor_breadth/2), -1.0 + this.sphere3_radius + this.vert_box_height + this.floor_depth/2, 0.0)
+        this.nail2.rotation.x = Math.PI/2
+        this.nail2.position.x += (this.sphere3_radius + this.rod_radius)
+        this.nail2.position.y += this.rod_height - this.nail2_axis_radius
+
+        this.rod.rotation.x = Math.PI/2
+        this.rod.position.z = this.nail2_axis_radius - this.rod_height/2
+
     }
 
     addDependencies()
@@ -116,7 +137,11 @@ export class Setup
 
         this.nail4.add(this.nail3);
 
-        this.animationSetup.add(this.sphere3)
+        this.animationSetup.add(this.sphere3);
+
+        this.animationSetup.add(this.nail2);
+
+        this.nail2.add(this.rod);
         
     }
 
